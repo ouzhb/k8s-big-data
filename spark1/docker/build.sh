be0bc24ba31851722eee69991106d186af6a1e92
m15651620957@163.com <m15651620957@163.com>
2018-12-17 19:19:46 +0800

    镜像瘦身！！

--- spark1/docker/build.sh
+++ spark1/docker/build.sh
@@ -3,4 +3,13 @@
 SPARK_VERSION="1.6.3"
 
 TAG="0.1.0-${SPARK_VERSION}"
+
+SPARK_FILE_NAME="spark-1.6.3-bin-with-hadoop-2.7.7"
+JAVA_FILE_NAME="jdk-8u191-linux-x64"
+tar -xvf packages/${SPARK_FILE_NAME}.tgz && mv ${SPARK_FILE_NAME} spark
+tar -xvf packages/${JAVA_FILE_NAME}.tar.gz
+
 docker build -t rg-bigdata/spark-standalone:$TAG ./
+
+rm -rf jdk1.8.0_191/
+rm -rf spark/
